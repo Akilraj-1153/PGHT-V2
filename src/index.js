5ebe2126df652ae3edc703dffef3a05979860b40
Akilraj-1153 <akilrajn1153@gmail.com>
2024-05-20 14:13:44 +0530

    index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
@@ -9,15 +9,12 @@ import { RecoilRoot } from 'recoil';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-    <RecoilRoot>
-      <App />
-    </RecoilRoot>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <RecoilRoot>
+        <App />
+      </RecoilRoot>
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
