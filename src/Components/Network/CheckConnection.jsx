adeacbbf4a85d03bddf85f90e181d094b005d8ca
Akilraj-1153 <akilrajn1153@gmail.com>
2024-05-23 12:49:05 +0530

    small fix

--- src/Components/Network/CheckConnection.jsx
+++ src/Components/Network/CheckConnection.jsx
@@ -1,17 +1,14 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Detector } from 'react-detect-offline';
-import gsap from 'gsap';
 
-const CheckConnection = ({ img,children }) => {
- 
-
+const CheckConnection = ({ img, children }) => {
   return (
     <Detector
       render={({ online }) => (
         online ? children : (
-          <div className='flex flex-col items-center justify-center w-screen h-screen gap-2 text-red-900 lg:text-2xl font-bold'>
-            <img className='h-[50%] w-auto' src={img}></img>
-            <h1 className='text-5xl'>You are currently offline</h1>
+          <div className='flex flex-col items-center justify-center w-screen h-screen gap-2 text-red-900 font-bold p-4'>
+            <img className='h-32 w-auto md:h-48 lg:h-64' src={img} alt='Offline Illustration' />
+            <h1 className='text-2xl md:text-3xl lg:text-5xl text-center'>You are currently offline</h1>
           </div>
         )
       )}
